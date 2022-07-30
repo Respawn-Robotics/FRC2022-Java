c4038c962e0a95073f85c5e39828e733868c5bdd
unknown <Administrator@BTDRL108692-28.Butlertech.org>
2022-07-30 10:49:52 -0400

    Project creation and CAN-ID constants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,43 @@
+package frc.robot;
+
+public final class Constants {
+
+    // CAN IDS
+
+    // PIDGEON
+    public static int PIDGEON = 20;
+    // SWERVE
+    // CANCODERS
+    public static int BLUE_FRONT_CANCODER = 23;
+    public static int BLUE_BACK_CANCODER = 24;
+    public static int ORANGE_FRONT_CANCODER = 22;
+    public static int ORANGE_BACK_CANCODER = 21;
+
+    // DRIVE
+    public static int BLUE_FRONT_DRIVE = 8;
+    public static int BLUE_BACK_DRIVE = 9;
+    public static int ORANGE_BACK_DRIVE = 4;
+    public static int ORANGE_FRONT_DRIVE = 5;
+
+    // TURN
+    public static int BLUE_FRONT_TURN = 7;
+    public static int BLUE_BACK_TURN = 10;
+    public static int ORANGE_BACK_TURN = 3;
+    public static int ORANGE_FRONT_TURN = 6;
+
+    // INTAKE
+    public static int INTAKE = 15;
+    public static int INTAKE_ACTUATE = 25;
+
+    // INDEXER
+    public static int ELEVATOR = 16;
+    public static int REJECT = 17;
+
+    // Shooter
+    public static int SHOOT = 19;
+    public static int TURRET = 18;
+
+    // Climb
+    public static int BLUE_CLIMB = 13;
+    public static int ORANGE_CLIMB = 11;
+}
